1f5551e16a5c79fb6a38647beb0499ccd8af1a89
Farzin Fadamin <ffadamin@gmail.com>
2013-07-19 15:07:56 +0430

    Recommit overlay fixes

--- javascript/default.js
+++ javascript/default.js
@@ -6,28 +6,27 @@ $(document).ready(function(){
 hd.core = {
 	init: function(){
 		$(".contact-us-link").on("click", $.proxy(this.showContactForm, this));
-		$(".overlay-contact .close").on("click", function(){
+		$(".overlay-inner .close").on("click", function(){
 			$(".overlay-holder").hide();
 		});
 		$(".overlay-holder").on("click", $.proxy(this.modalClick, this));
 	},
-	
+
 	showContactForm: function(e){
 		e.preventDefault();
 		this.resetContactForm();
 		$(".overlay-holder").show();
 	},
-	
+
 	resetContactForm: function(){
 		$(".overlay-contact").find(":text, textarea").val("");
 		$(".overlay-contact").find(".error").removeClass("error");
 	},
-	
+
 	modalClick: function(e){
 		if(!$(e.target).closest(".overlay-contact").length){
 			$(".overlay-contact .close").trigger("click");
 		}
 	}
-	
-};
 
+};
